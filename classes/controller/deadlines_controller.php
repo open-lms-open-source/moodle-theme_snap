135fad4304285c51d6ae078b348ea35373631c89
Mark Nielsen <mark@moodlerooms.com>
2014-07-28 11:12:13 -0700

    INT-6451: Adding some routing and controller classes

---
+++ classes/controller/deadlines_controller.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Deadlines Controller
+ *
+ * @package   theme_snap
+ * @copyright Copyright (c) 2013 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace theme_snap\controller;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Handles requests regarding user deadlines.
+ *
+ * @package   theme_snap
+ * @copyright Copyright (c) 2013 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class deadlines_controller extends controller_abstract {
+    /**
+     * Do any security checks needed for the passed action
+     *
+     * @param string $action
+     */
+    public function require_capability($action) {
+        // TODO: Implement require_capability() method.
+    }
+
+    /**
+     * Get the user's deadlines.
+     *
+     * @return string
+     */
+    public function get_deadlines_action() {
+        // TODO: Generate response and return a string.
+    }
+}
---
+++ rest.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Snap AJAX handler
+ *
+ * @package   theme_snap
+ * @copyright Copyright (c) 2013 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use theme_snap\controller\kernel;
+use theme_snap\controller\deadlines_controller;
+use theme_snap\controller\router;
+
+define('AJAX_SCRIPT', true);
+define('NO_DEBUG_DISPLAY', true);
+
+require_once(__DIR__.'/../../config.php');
+
+$systemcontext = context_system::instance();
+
+$action    = required_param('action', PARAM_ALPHAEXT);
+$contextid = optional_param('contextid', $systemcontext->id, PARAM_INT);
+
+list($context, $course, $cm) = get_context_info_array($contextid);
+
+require_login($course, false, $cm, false, true);
+
+/** @var $PAGE moodle_page */
+$PAGE->set_context($context);
+$PAGE->set_url('/theme/snap/rest.php', array('action' => $action, 'contextid' => $context->id));
+
+$router = new router();
+$router->add_controller(new deadlines_controller());
+
+$kernel = new kernel($router);
+$kernel->handle($action);
