2357f5ef3848cd5f8b869258889210ff5c4b9105
gthomas2 <gthomas@moodlerooms.com>
2015-03-10 14:33:17 +0000

    INT-7302,INT-7382: Fix comment

--- model/handler.php
+++ model/handler.php
@@ -15,11 +15,9 @@ class theme_snap_model_handler {
 
 
     /**
-     * The course update event
+     * The course update event.
      *
-     * Only trick here is that it must reverse engineer
-     * the category.  On failure, the category is unset
-     * and whatever is in conduit remains unmodified
+     * process cover image.
      *
      * @param course_updated $event
      * @return void
