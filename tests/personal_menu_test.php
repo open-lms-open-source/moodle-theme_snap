0958883f7ec50e7b9df09cc3fd3e575a872ddaa9
gthomas2 <gthomas@moodlerooms.com>
2015-08-27 12:28:59 +0100

    INT-7844: Added tests for hsuforum

--- tests/personal_menu_test.php
+++ tests/personal_menu_test.php
@@ -35,10 +35,59 @@ defined('MOODLE_INTERNAL') || die();
  */
 class theme_snap_personal_menu_test extends \advanced_testcase {
 
+    protected $user1;
+    protected $user2;
+    protected $course1;
+    protected $course2;
+    protected $groupA;
+    protected $groupB;
+    
     public function setUp() {
+        global $CFG, $DB;
+        
         // We must clear the subscription caches. This has to be done both before each test, and after in case of other
         // tests using these functions.
         \mod_forum\subscriptions::reset_forum_cache();
+
+
+        require_once($CFG->dirroot . '/mod/forum/lib.php');
+
+        $this->resetAfterTest();
+
+        $this->user1 = $this->getDataGenerator()->create_user();
+        $this->user2 = $this->getDataGenerator()->create_user();
+
+        $this->course1 = $this->getDataGenerator()->create_course();
+        $this->course2 = $this->getDataGenerator()->create_course();
+
+        $sturole = $DB->get_record('role', array('shortname'=>'student'));
+
+        // Enrol user1 to both courses but user2 only to course2.
+        $this->getDataGenerator()->enrol_user($this->user1->id,
+            $this->course1->id,
+            $sturole->id);
+        $this->getDataGenerator()->enrol_user($this->user1->id,
+            $this->course2->id,
+            $sturole->id);
+        $this->getDataGenerator()->enrol_user($this->user2->id,
+            $this->course2->id,
+            $sturole->id);
+
+        // Add 2 groups to course2.
+        $this->groupA = $this->getDataGenerator()->create_group([
+            'courseid' => $this->course2->id,
+            'name' => 'A'
+        ]);
+        $this->groupB = $this->getDataGenerator()->create_group([
+            'courseid' => $this->course2->id,
+            'name' => 'B'
+        ]);
+
+        // Add user1 to both groups but user2 to just groupA.
+        groups_add_member($this->groupA->id, $this->user1);
+        groups_add_member($this->groupB->id, $this->user1);
+        groups_add_member($this->groupA->id, $this->user2);
+
     }
 
     public function tearDown() {
@@ -52,166 +101,142 @@ class theme_snap_personal_menu_test extends \advanced_testcase {
      * @throws \coding_exception
      */
     public function test_forums() {
-        global $DB, $CFG;
 
-        require_once($CFG->dirroot . '/mod/forum/lib.php');
+        $this->do_forum_type('forum');
+        $this->do_forum_type('hsuforum', 11, 7);
 
-        $this->resetAfterTest();
+    }
 
-        $user1 = $this->getDataGenerator()->create_user();
-        $user2 = $this->getDataGenerator()->create_user();
+    /**
+     * @param string $ftype - forum or hsuforum
+     * @param int $u1offset
+     * @param int $u2offset
+     * @throws \coding_exception
+     */
+    public function do_forum_type($ftype, $u1offset =0, $u2offset = 0) {
 
-        $course1 = $this->getDataGenerator()->create_course();
-        $course2 = $this->getDataGenerator()->create_course();
-
-        $sturole = $DB->get_record('role', array('shortname'=>'student'));
-
-        // Enrol user1 to both courses but user2 only to course2.
-        $this->getDataGenerator()->enrol_user($user1->id,
-            $course1->id,
-            $sturole->id);
-        $this->getDataGenerator()->enrol_user($user1->id,
-            $course2->id,
-            $sturole->id);
-        $this->getDataGenerator()->enrol_user($user2->id,
-            $course2->id,
-            $sturole->id);
-
-        // There are no forums to start with, check activity array is empty.
-        $activity = local::recent_forum_activity($user1->id);
-        $this->assertEmpty($activity);
+        if ($u1offset === 0 && $u2offset ===0) {
+            // There are no forums to start with, check activity array is empty.
+            $activity = local::recent_forum_activity($this->user1->id);
+            $this->assertEmpty($activity);
+        }
 
         // Create 2 regular forums, one in each course.
         $record = new \stdClass();
-        $record->course = $course1->id;
-        $forum1 = $this->getDataGenerator()->create_module('forum', $record);
+        $record->course = $this->course1->id;
+        $forum1 = $this->getDataGenerator()->create_module($ftype, $record);
 
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $forum2 = $this->getDataGenerator()->create_module('forum', $record);
+        $record->course = $this->course2->id;
+        $forum2 = $this->getDataGenerator()->create_module($ftype, $record);
 
         // Add discussion to course 1 started by user1.
         $record = new \stdClass();
-        $record->course = $course1->id;
-        $record->userid = $user1->id;
+        $record->course = $this->course1->id;
+        $record->userid = $this->user1->id;
         $record->forum = $forum1->id;
-        $discussion1 = $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($record);
+        $discussion1 = $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_discussion($record);
 
         // Add discussion to course 2 started by user1.
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $record->userid = $user1->id;
+        $record->course = $this->course2->id;
+        $record->userid = $this->user1->id;
         $record->forum = $forum2->id;
-        $discussion2 = $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($record);
+        $discussion2 = $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_discussion($record);
 
         // Add discussions to course2 started by user2.
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $record->userid = $user2->id;
+        $record->course = $this->course2->id;
+        $record->userid = $this->user2->id;
         $record->forum = $forum2->id;
-        $discussion3 = $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($record);
+        $discussion3 = $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_discussion($record);
 
         // Add post to forum1 and 2 by user1.
         $record = new \stdClass();
-        $record->course = $course1->id;
-        $record->userid = $user1->id;
+        $record->course = $this->course1->id;
+        $record->userid = $this->user1->id;
         $record->forum = $forum1->id;
         $record->discussion = $discussion1->id;
-        $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
+        $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_post($record);
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $record->userid = $user2->id;
+        $record->course = $this->course2->id;
+        $record->userid = $this->user2->id;
         $record->forum = $forum2->id;
         $record->discussion = $discussion2->id;
-        $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
+        $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_post($record);
 
         // Add post to forum2 by user1.
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $record->userid = $user2->id;
+        $record->course = $this->course2->id;
+        $record->userid = $this->user2->id;
         $record->forum = $forum2->id;
         $record->discussion = $discussion2->id;
-        $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
+        $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_post($record);
 
         // Note: In testing number of posts, discussions are counted too as there is a post for each discussion created.
 
         // Test user1 viewable posts.
-        $activity = local::recent_forum_activity($user1->id);
+        $activity = local::recent_forum_activity($this->user1->id);
         // Should be 6 posts.
-        $this->assertEquals(6, count($activity));
+        $this->assertEquals($u1offset+6, count($activity));
 
         // Test user2 viewable posts.
-        $activity = local::recent_forum_activity($user2->id);
+        $activity = local::recent_forum_activity($this->user2->id);
         // Should be 4 posts - user2 is not enrolled on course1.
-        $this->assertEquals(4, count($activity));
-
-        // Add 2 groups to course2.
-        $groupA = $this->getDataGenerator()->create_group([
-            'courseid' => $course2->id,
-            'name' => 'A'
-        ]);
-        $groupB = $this->getDataGenerator()->create_group([
-            'courseid' => $course2->id,
-            'name' => 'B'
-        ]);
-
-        // Add user1 to both groups but user2 to just groupA.
-        groups_add_member($groupA->id, $user1);
-        groups_add_member($groupB->id, $user1);
-        groups_add_member($groupA->id, $user2);
+        $this->assertEquals($u2offset+4, count($activity));
 
         // Create a forum with group mode enabled.
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $forum3 = $this->getDataGenerator()->create_module('forum', $record, ['groupmode' => SEPARATEGROUPS]);
+        $record->course = $this->course2->id;
+        $forum3 = $this->getDataGenerator()->create_module($ftype, $record, ['groupmode' => SEPARATEGROUPS]);
 
         // Add a discussion and 2 posts for groupA users.
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $record->userid = $user1->id;
+        $record->course = $this->course2->id;
+        $record->userid = $this->user1->id;
         $record->forum = $forum3->id;
-        $record->groupid = $groupA->id;
-        $discussion4 = $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($record);
+        $record->groupid = $this->groupA->id;
+        $discussion4 = $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_discussion($record);
 
         // (At this point - 7 posts for user1, 5 for user2).
 
         for ($p=1; $p<=2; $p++) {
             // Create 1 post by user1 and user2.
-            $user = $p==1 ? $user1 : $user2;
+            $user = $p==1 ? $this->user1 : $this->user2;
             $record = new \stdClass();
-            $record->course = $course2->id;
+            $record->course = $this->course2->id;
             $record->userid = $user->id;
             $record->forum = $forum3->id;
             $record->discussion = $discussion4->id;
-            $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
+            $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_post($record);
         }
 
         // (At this point - 9 posts for user1, 7 for user2).
 
         // Add a discussion and 1 post for groupB users.
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $record->userid = $user1->id;
+        $record->course = $this->course2->id;
+        $record->userid = $this->user1->id;
         $record->forum = $forum3->id;
-        $record->groupid = $groupB->id;
-        $discussion5 = $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($record);
+        $record->groupid = $this->groupB->id;
+        $discussion5 = $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_discussion($record);
         $record = new \stdClass();
-        $record->course = $course2->id;
-        $record->userid = $user1->id;
+        $record->course = $this->course2->id;
+        $record->userid = $this->user1->id;
         $record->forum = $forum3->id;
         $record->discussion = $discussion5->id;
-        $this->getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
+        $this->getDataGenerator()->get_plugin_generator('mod_'.$ftype)->create_post($record);
 
         // (At this point - 11 posts for user1, 7 for user2).
 
         // Check user1.
-        $activity = local::recent_forum_activity($user1->id);
-        // Should be 11 posts.
-        $this->assertEquals(11, count($activity));
+        $activity = local::recent_forum_activity($this->user1->id);
+        // Should be offset + 11 posts.
+        $this->assertEquals($u1offset+11, count($activity));
 
         // Check user2.
-        $activity = local::recent_forum_activity($user2->id);
-        // Should be 7 posts.
-        $this->assertEquals(7, count($activity));
+        $activity = local::recent_forum_activity($this->user2->id);
+        // Should be offset + 7 posts.
+        $this->assertEquals($u2offset+7, count($activity));
     }
 }
