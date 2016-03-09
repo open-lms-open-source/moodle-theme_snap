73d7bb5e0bc5b5ac13a9a12f190a1a43af6d15a3
Rafael Monterroza <rafael.monterroza@blackboard.com>
2016-03-09 20:54:53 +0000

    INT-9134: Logic fixed in gradebook_accessible

--- renderers/snap_shared.php
+++ renderers/snap_shared.php
@@ -412,9 +412,9 @@ class snap_shared extends renderer_base {
     public static function gradebook_accessible($context) {
         global $COURSE;
 
-        //Ask if user has not capabilities and course is set to not show grades to students.
+        // Ask if user has not capabilities and if course is set to not to show the grades to students.
         if ((!has_capability('gradereport/grader:view', $context)) && ($COURSE->showgrades == 0)){
-            return null;
+            return false;
         }
         // Find all enabled reports.
         $reports = core_component::get_plugin_list('gradereport');
@@ -548,7 +548,7 @@ class snap_shared extends renderer_base {
             // Gradebook.
             $links[] = array(
                 'link' => 'grade/index.php?id='.$COURSE->id,
-                'title' => $gradebookicon.get_string('gradebook', 'grades')  
+                'title' => $gradebookicon.get_string('gradebook', 'grades')
             );
         }
 
