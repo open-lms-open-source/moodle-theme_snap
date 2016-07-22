4a8757053de20d259d70c67327cb517a7cd53562
Mark Nielsen <mark@moodlerooms.com>
2016-07-22 13:58:36 -0700

    INT-9966: theme_snap fix JS console error

--- amd/src/section_asset_movement.js
+++ amd/src/section_asset_movement.js
@@ -369,7 +369,7 @@ define(['jquery', 'core/log', 'core/templates', 'core/notification'], function($
                     // This should never happen, but just in case...
                     $('.section-moving').removeClass('section-moving');
                     section.addClass('section-moving');
-                    $('a[href$=#section-' + sectionid + ']').parent('li').addClass('section-moving');
+                    $('a[href$="#section-' + sectionid + '"]').parent('li').addClass('section-moving');
                     $('body').addClass('snap-move-section');
 
                     var title = M.util.get_string('moving', 'theme_snap', sectionname);
