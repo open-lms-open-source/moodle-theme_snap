547e29cea530c46310e2148d48a294e9432c1958
Guillermo Alvarez <guillermo.alvarez@blackboard.com>
2017-01-16 17:51:17 -0500

    INT-11063: Blacklist H5P module to not use snap_page_requirements_manager

--- config.php
+++ config.php
@@ -214,9 +214,12 @@ if ($themeissnap && $notajaxscript) {
         $PAGE->initialise_theme_and_output();
 
         // Modify $PAGE to use snap requirements manager.
-        $requires = new ReflectionProperty($PAGE, '_requires');
-        $requires->setAccessible(true);
-        $requires->setValue($PAGE, new snap_page_requirements_manager());
-        $requires->setAccessible(false);
+        $blacklist = ['page-mod-hvp-view' => 'page-mod-hvp-view'];
+        if (!array_key_exists($PAGE->bodyid, $blacklist)) {
+            $requires = new ReflectionProperty($PAGE, '_requires');
+            $requires->setAccessible(true);
+            $requires->setValue($PAGE, new snap_page_requirements_manager());
+            $requires->setAccessible(false);
+        }
     }
 }
