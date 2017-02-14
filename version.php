960ea136a3a03ee13cf43da7fab8ef2743e591c7
Mark Nielsen <mark@moodlerooms.com>
2017-02-14 08:55:17 -0800

    INT-11133: bump requires and release for Moodle 3.2.1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2016121304;
-$plugin->requires  = 2016052300;
-$plugin->release   = '3.1.3';
+$plugin->requires  = 2016120500;
+$plugin->release   = '3.2.1';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_snap';
