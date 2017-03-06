9e147888eeda069f4e47439d742ff33bed6578e0
stuart lamour <slamour@moodlerooms.com>
2017-03-06 17:45:21 +0000

    INT-11343: delete duplicate line

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -856,7 +856,6 @@ class core_renderer extends toc_renderer {
             $menu = '<span class="hidden-xs">' .get_string('menu', 'theme_snap'). '</span>';
             $badge = $this->render_badge_count();
             $linkcontent = $menu.$picture.$badge;
-            $linkcontent = $menu.$picture.$badge;
             $attributes = array(
                 'aria-haspopup' => 'true',
                 'class' => 'js-personal-menu-trigger snap-my-courses-menu',
