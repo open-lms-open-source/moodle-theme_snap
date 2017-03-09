014f6aad44e6d10fcba563d4ab098c16feb0c755
Sam Chaffee <sam@moodlerooms.com>
2017-03-09 09:57:58 -0700

    Fix bad conflict resolution in Snap version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016121304;
+$plugin->version   = 2016121305;
 $plugin->requires  = 2016120500;
 $plugin->release   = '3.2.1';
 $plugin->maturity  = MATURITY_STABLE;
