a9735688044c5d8859f61aa12dac2bc101c43361
Mark Nielsen <mark@moodlerooms.com>
2017-06-30 09:08:54 -0700

    INT-11942: Bump version and release

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017041700;
+$plugin->version   = 2017063000;
 $plugin->requires  = 2016120500;
-$plugin->release   = '3.2.2';
+$plugin->release   = '3.2.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_snap';
 $plugin->dependencies = [
