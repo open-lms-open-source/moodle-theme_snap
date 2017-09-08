e6f2b2d37ff12af4c6ce53c98d50c4302ad17f94
stuart lamour <slamour@moodlerooms.com>
2017-09-08 14:09:32 +0100

    INT-12296: toc default to list

--- settings/course_settings.php
+++ settings/course_settings.php
@@ -4,21 +4,13 @@ use theme_snap\admin_setting_configradiobuttons;
 
 $snapsettings = new admin_settingpage('themesnapcoursedisplay', get_string('coursedisplay', 'theme_snap'));
 
-// Left toc option.
-$name = 'theme_snap/leftnav';
-$title = new lang_string('leftnav', 'theme_snap');
-$description = new lang_string('leftnavdesc', 'theme_snap');
-$default = $unchecked;
-$setting = new admin_setting_configcheckbox($name, $title, $description, $default, $checked, $unchecked);
-$snapsettings->add($setting);
-
 // Course toc display options.
 $name = 'theme_snap/leftnav';
 $title = new lang_string('leftnav', 'theme_snap');
 $list = get_string('list', 'theme_snap');
 $top = get_string('top', 'theme_snap');
 $radios = array('list' => $list, 'top' => $top);
-$default = 'top';
+$default = 'list';
 $description = new lang_string('leftnavdesc', 'theme_snap');
 $setting = new admin_setting_configradiobuttons($name, $title, $description, $default, $radios);
 $snapsettings->add($setting);
