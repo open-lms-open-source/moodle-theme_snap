fc32e2e04b1610dfcce76394a566b695fd9e7a6f
Sam Chaffee <sam@moodlerooms.com>
2017-11-30 11:57:22 -0700

    INT-12686: Importing ca lang

---
+++ lang/ca/theme_snap.php
@@ -0,0 +1,236 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2017 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['aboutcourse'] = 'Quant a aquest curs';
+$string['activity'] = 'Activitat';
+$string['action:changeassetvisibility'] = 'canvia la visibilitat del recurs';
+$string['action:duplicateasset'] = 'duplica el recurs';
+$string['action:changesectionvisibility'] = 'canvia la visibilitat de la secció';
+$string['action:highlightsectionvisibility'] = 'destaca la visibilitat de la secció';
+$string['addanewsection'] = 'Crea una secció nova';
+$string['addresourceoractivity'] = 'Crea activitats d\'aprenentatge';
+$string['admin'] = 'Admin';
+$string['advancedbrandingheading'] = 'Construcció de marca avançada';
+$string['ago'] = 'enrere';
+$string['alerts'] = 'Alertes';
+$string['answered'] = 'Contestades';
+$string['appendices'] = 'Eines';
+$string['assigndraft'] = 'L’esborrany requereix la vostra confirmació';
+$string['assignreopened'] = 'Reoberta';
+$string['at'] = 'a';
+$string['attempted'] = 'Intentat';
+$string['brandingheading'] = 'Construcció de marca';
+$string['browse'] = 'Explora';
+$string['browseallcourses'] = 'Navega per tots els cursos';
+$string['cachedef_course_completion_progress'] = 'Això s’utilitza per completar les dades de memòria cau per curs o per usuari.';
+$string['cachedef_course_completion_progress_ts'] = 'Això s’utilitza perquè puguem invalidar la memòria cau a nivell de sessió si canvien els paràmetres de compleció de curs per a un determinat curs o mòdul.';
+$string['cachedef_webservicedefinitions'] = 'Emmagatzematge de la memòria cau per a les definicions autogenerades del servei web.';
+$string['changecoverimage'] = 'Canvia la imatge de portada';
+$string['changefullname'] = 'Canvia el nom del lloc';
+$string['chapters'] = 'Capítols';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h2>Ajusta</h2><img class=img-polaroid src="snap/pix/screenshot.jpg" />';
+$string['close'] = 'Tanca';
+$string['conditional'] = 'Condicional';
+$string['contents'] = 'Continguts';
+$string['contributed'] = 'Contribuït';
+$string['courses'] = 'Cursos';
+$string['coursecontacts'] = 'Contactes del curs';
+$string['coursefootertoggle'] = 'Mostra el peu de pàgina del curs.';
+$string['coursefootertoggledesc'] = 'El peu de pàgina del curs mostra informació útil als usuaris en una pàgina del curs que inclou els contactes, la descripció i l’activitat recent del curs.';
+$string['courseformatnotification'] = 'El format actual del curs que feu servir no és totalment compatible amb el tema Ajusta. Per a obtenir la millor experiència, Moodlerooms recomana utilitzar els formats de curs per Tema o Setmanals amb el tema Ajusta. El format del curs es pot canviar als <a href="{$a}">paràmetres del curs</a>.';
+$string['coursefixydefaulttext'] = 'Actualment no us heu matriculat a cap curs.<br>Els cursos als quals us matriculeu es mostraran aquí.';
+$string['coursenavigation'] = 'Navegació del curs';
+$string['coursesummaryfilesunsuitable'] = 'Buideu els fitxers de resum abans d’intentar canviar la imatge de portada';
+$string['coursetools'] = 'Eines del curs';
+$string['createsection'] = 'Crea una secció';
+$string['current'] = 'Actual';
+$string['customcss'] = 'Personalitza CSS';
+$string['customcssdesc'] = 'Afegeix qualsevol CSS addicional per al vostre lloc.';
+$string['deadlines'] = 'Dates límit';
+$string['deadlinestoggle'] = 'Dates límit';
+$string['deadlinestoggledesc'] = 'Les dates límit mostren totes les dates límit d\'activitats properes dels cursos a què us heu matriculat.';
+$string['defaultsummary'] = 'Utilitzeu aquesta àrea per descriure sobre què tracta aquest tema (amb text, imatges, àudio i vídeo).';
+$string['defaultintrosummary'] = 'Benvingut al vostre curs nou {$a}.
+<br>Comenceu per descriure de què tracta el curs amb text, imatges, àudio i vídeo.';
+$string['defaulttopictitle'] = 'Tema sense títol';
+$string['debugerrors'] = 'Errors de depuració';
+$string['deleteassetconfirm'] = 'Esborra {$a}';
+$string['deletingasset'] = 'S\'està suprimint {$a}';
+$string['deletingassetname'] = 'S’està suprimint {$a->type} "{$a->name}"';
+$string['deletesectionconfirm'] = 'Esborra la secció';
+$string['deletingsection'] = 'S’està suprimint "{$a}"';
+$string['draft'] = 'No publicat per als estudiants';
+$string['dropzonelabel'] = 'Deixeu anar fitxers per adjuntar o <span class="btn-link">navegueu</span>';
+$string['due'] = 'Data límit';
+$string['edit'] = 'Edita "{$a}"';
+$string['editcoursecontent'] = 'Edita els blocs';
+$string['editcoursesettings'] = 'Paràmetres del curs';
+$string['editcoursetopic'] = 'Edita la secció';
+$string['editcustomfooter'] = 'Edita el peu de pàgina';
+$string['editcustommenu'] = 'Edita el menú personalitzat';
+$string['error:coverimageexceedsmaxbytes'] = 'La imatge de portada supera la mida màxima de fitxer permesa al nivell de lloc ({$a})';
+$string['error:coverimageresolutionlow'] = 'Si voleu que la qualitat sigui millor, us recomanem una imatge més gran de com a mínim 1024px d’ample.';
+$string['error:failedtochangeassetvisibility'] = 'No s’ha pogut ocultar o mostrar el recurs';
+$string['error:failedtochangesectionvisibility'] = 'No s’ha pogut ocultar o mostrar la secció';
+$string['error:failedtohighlightsection'] = 'No s’ha pogut destacar la secció';
+$string['error:failedtoduplicateasset'] = 'No s’ha pogut duplicar';
+$string['facebook'] = 'Facebook';
+$string['facebookdesc'] = 'L’URL de la vostra pàgina de Facebook';
+$string['favicon'] = 'Icona de web';
+$string['favicondesc'] = 'Pengeu la vostra icona de web en format .ico aquí. La icona de web es mostrarà juntament amb el títol de pàgina a la barra de pestanyes del navegador i als marcadors.';
+$string['favorite'] = 'Preferit {$a}';
+$string['favorited'] = 'Marcat com a preferit {$a}';
+$string['featurespots'] = 'Espots de presentació de funcions i avantatges';
+$string['featurespotsedit'] = 'Edita els espots de presentació de funcions';
+$string['featurespotshelp'] = 'Els espots de presentació de funcions serveixen per destacar els avantatges del vostre lloc i els avantatges principals per als usuaris actuals o potencials.
+Els espots de presentació de funcions són perfectes per descriure les funcions o els avantatges principals del vostre lloc de manera resumida,
+i es mostrarà sota la imatge de capçalera de la pàgina principal del vostre lloc.<br>
+Podeu afegir fins a 3 espots; només cal que afegiu el títol, contingut i una imatge opcional. Totes les imatges que utilitzeu no poden superar els 200px d’amplada i han de tenir la mateixa alçada.';
+$string['featurespotsheading'] = 'Capçalera de presentació de funcions';
+$string['featureonetitle'] = 'Títol de la funció 1';
+$string['featuretwotitle'] = 'Títol de la funció 2';
+$string['featurethreetitle'] = 'Títol de la funció 3';
+$string['featureonetext'] = 'Contingut de la funció 1';
+$string['featuretwotext'] = 'Contingut de la funció 2';
+$string['featurethreetext'] = 'Contingut de la funció 3';
+$string['featureoneimage'] = 'Imatge de la funció 1';
+$string['featuretwoimage'] = 'Imatge de la funció 2';
+$string['featurethreeimage'] = 'Imatge de la funció 3';
+$string['feedbackavailable'] = 'Comentaris disponibles';
+$string['feedbacktoggle'] = 'Comentaris recents';
+$string['feedbacktoggledesc'] = 'Comentaris recents mostra tots els comentaris recents cap als aprenents i les trameses que han de qualificar als educadors.';
+$string['footnote'] = 'Peu de pàgina del lloc';
+$string['footnotedesc'] = 'Un peu de pàgina per al vostre lloc. Aquest és el millor lloc per posar enllaços d’ajuda, assistència i de qualsevol altre lloc que tingui la vostra organització que voleu coneguin que els aprenents o educadors, com ara la biblioteca o el correu electrònic.';
+$string['forcepwdwarningpersonalmenu'] = 'Heu de <a href="{$a}">canviar la contrasenya</a> abans de fer servir el menú personal.';
+$string['forumauthor'] = 'Autor';
+$string['forumlastpost'] = 'Últim apunt';
+$string['forumpicturegroup'] = 'Grup';
+$string['forumreplies'] = 'Respostes';
+$string['forumtopic'] = 'Tema';
+$string['forumposts'] = 'Missatges al fòrum';
+$string['forumpoststoggle'] = 'Missatges al fòrum';
+$string['forumpoststoggledesc'] = 'Els 10 apunts més recents al fòrum de tots els cursos al què esteu matriculat es mostraran al menú personal.';
+$string['fullname'] = 'Nom del lloc';
+$string['fullnamedesc'] = 'El nom del vostre lloc.';
+$string['graderadviseuserreport'] = 'L’"informe del qualificador" no funciona bé en dispositius mòbils. Es recomana fer servir l’"informe d’usuari"';
+$string['grading'] = 'S\'està qualificant';
+$string['headingfont'] = 'Font de la capçalera';
+$string['headingfont_desc'] = 'Aquesta font es fa servir per a les capçaleres (elements h1-h6) per tot el vostre lloc. Si voleu incloure una font web, recordeu afegir-la al formulari d’html addicional de moodle.';
+$string['help'] = 'Ajuda';
+$string['helpwithlogin'] = 'Ajuda amb l’inici de sessió';
+$string['helpwithloginandguest'] = 'Ajuda amb l’inici de sessió / accés de convidat';
+$string['hidenavblock'] = 'Oculta el bloc de navegació de Moodle predeterminat';
+$string['hidenavblockdesc'] = 'Amb Ajusta, us recomanem que oculteu el bloc de navegació de Moodle predeterminat. ';
+$string['hiddencoursestoggle'] = 'Cursos ocults ({$a})';
+$string['highlightedsection'] = 'destacat';
+$string['home'] = 'Inici';
+$string['image'] = 'imatge';
+$string['instagram'] = 'Instagram';
+$string['instagramdesc'] = 'L’URL del vostre compte d’Instagram.';
+$string['introduction'] = 'Introducció';
+$string['knowledgebase'] = 'Base de coneixements de Moodlerooms';
+$string['loading'] = 'S\'està carregant...';
+$string['loggedinasguest'] = 'Heu iniciat sessió com a convidat';
+$string['loggedoutmsg'] = 'Ara no teniu la sessió iniciada. Si voleu continuar fent servir aquest lloc, torneu a iniciar la sessió.';
+$string['loggedoutmsgtitle'] = 'No teniu la sessió iniciada.';
+$string['loggedoutfailmsg'] = 'Cal que inicieu la sessió per {$a}.';
+$string['loginform'] = 'Inici de sessió';
+$string['logo'] = 'Logotip';
+$string['logodesc'] = 'Pengeu el vostre logotip personalitzat aquí. El logotip es mostrarà a la capçalera a tot el lloc.';
+$string['menu'] = 'Els meus cursos';
+$string['messageread'] = 'Missatge llegit';
+$string['messages'] = 'Missatges';
+$string['messagestoggle'] = 'Missatges';
+$string['messagestoggledesc'] = 'Missatges mostra els missatges més recents d’altres usuaris de les darreres 12 setmanes.';
+$string['more'] = 'Més';
+$string['morenews'] = 'Més notícies';
+$string['movingstartedhelp'] = 'Navegueu fins on voleu col·locar la secció "{$a}"';
+$string['movingdropsectionhelp'] = 'Col·loqueu la secció "{$a->moving}" abans de la secció "{$a->before}"';
+$string['moving'] = 'S’està movent "{$a}"';
+$string['movingcount'] = 'S’estan movent {$a} objectes';
+$string['movefailed'] = 'No s’han pogut moure "{$a}"';
+$string['move'] = 'Mou "{$a}"';
+$string['movehere'] = 'Mou aquí';
+$string['movesection'] = 'Mou la secció';
+$string['nextsection'] = 'Secció següent';
+$string['nodeadlines'] = 'No teniu cap data límit pendent.';
+$string['noforumposts'] = 'No teniu cap apunt al fòrum que sigui rellevant.';
+$string['nograded'] = 'No teniu cap comentari recent.';
+$string['nograding'] = 'No teniu cap tramesa per qualificar.';
+$string['nomessages'] = 'No teniu missatges.';
+$string['notanswered'] = 'Sense resposta';
+$string['notattempted'] = 'No intentat';
+$string['notcontributed'] = 'No contribuït';
+$string['notpublished'] = 'No publicat per als estudiants';
+$string['notsubmitted'] = 'No tramès';
+$string['overdue'] = 'Venciment';
+$string['personalmenu'] = 'Funcions de menú personal';
+$string['personalmenulogintoggle'] = 'Mostra el menú personal a l’inici de sessió';
+$string['personalmenulogintoggledesc'] = 'Obre el menú personal immediatament després d’iniciar la sessió';
+$string['pld'] = 'PLD';
+$string['pluginname'] = 'Ajusta';
+$string['poster'] = 'Imatge de portada';
+$string['posterdesc'] = 'Una imatge gran de portada per a la pàgina principal del vostre lloc. La imatge ha d’excloure el text. És recomanable que feu servir imatges de 1280px d’amplada per 500px d’alçada (fins a 2000px d’amplada) Si optimitzeu la vostra imatge per a la web, la pàgina es carregarà més ràpidament.';
+$string['poweredbyrunby'] = 'Creat {$a} amb <a href="http://moodlerooms.com/" target="_blank">Moodlerooms</a>, amb tecnologia de <a href="http://www.moodle.com/" target="_blank">Moodle</a>.';
+$string['previoussection'] = 'Secció anterior';
+$string['problemsfound'] = 'S’han trobat problemes';
+$string['readmore'] = 'Més informació&nbsp;»';
+$string['recentactivity'] = 'Activitat recent';
+$string['recentfeedback'] = 'Retroacció';
+$string['region-main'] = 'Principal';
+$string['region-side-main-box'] = 'Principal';
+$string['region-side-post'] = 'Dret';
+$string['region-side-pre'] = 'Esquerre';
+$string['region-side-top'] = 'Capdamunt';
+$string['released'] = 'Publicat: {$a}';
+$string['reopened'] = 'Reoberta';
+$string['seriffont'] = 'Tipus de lletra Serif';
+$string['seriffont_desc'] = 'Aquesta font s’utilitza per a la major part del contingut generat per l’usuari. Si utilitzeu una font Serif, el contingut generat per l’usuari és més fàcil de llegir i sembla com si l’hagués escrit un humà.';
+$string['submitted'] = 'Lliurat';
+$string['subtitle'] = 'Descripció del lloc';
+$string['subtitle_desc'] = 'Una línia d\'etiquetes breu sobre el vostre lloc, d’uns 240 caràcters. La descripció del vostre lloc pot mostrar-se a la pàgina principal.';
+$string['themecolor'] = 'Color de la marca';
+$string['themecolordesc'] = 'El color primari del vostre lloc que s’utilitza al fons dels enllaços, botons i menús. Els colors brillants queden millor i proporcionen al vostre lloc un aspecte modern.';
+$string['title'] = 'Títol';
+$string['togglenavigation'] = 'Commuta la navegació';
+$string['topicactions'] = 'Accions de tema';
+$string['twitter'] = 'Twitter';
+$string['twitterdesc'] = 'L’URL del vostre compte de Twitter.';
+$string['unenrolme'] = 'Cancel·la la matrícula';
+$string['unread'] = 'no llegit';
+$string['unsupportedcoverimagetype'] = 'El tipus d’imatge de portada no és compatible ({$a})';
+$string['via'] = 'via';
+$string['viewcourse'] = 'Mostra el curs';
+$string['viewyourprofile'] = 'Mostra el vostre perfil';
+$string['viewmyfeedback'] = 'Mostra els meus comentaris';
+$string['viewcalendar'] = 'Mostra el meu calendari';
+$string['viewforumposts'] = 'Mostra els meu apunts als fòrums';
+$string['viewmessaging'] = 'Mostra els meus missatges';
+$string['warnsiteformatflexpage'] = 'El vostre lloc fa servir Flexpage a la pàgina principal. Flexpage no està optimitzat per a dispositius mòbils amb pantalles petites. Us recomanem que desactiveu Flexpage a la <a href="{$a}">configuració de la pàgina principal</a> si voleu que el vostre lloc estigui adaptat per a mòbils.';
+$string['xofyanswered'] = '{$a->completed} de {$a->participants} contestades';
+$string['xofyattempted'] = '{$a->completed} de {$a->participants} intentades';
+$string['xofycontributed'] = '{$a->completed} de {$a->participants} contribuïdes';
+$string['xofysubmitted'] = '{$a->completed} de {$a->participants} trameses';
+$string['xungraded'] = '{$a} No qualificat';
+$string['youtube'] = 'Youtube';
+$string['youtubedesc'] = 'L’URL del vostre canal de YouTube';
