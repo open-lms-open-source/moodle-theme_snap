38f276b004514fb97784c4332b79a6ceef91175e
vagrant <vagrant@vagrant.vm>
2018-01-24 16:45:10 +0000

    INT-12838: Importing ar lang

---
+++ lang/ar/theme_snap.php
@@ -0,0 +1,208 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['aboutcourse'] = 'حول هذا المقرر الدراسي';
+$string['activity'] = 'النشاط';
+$string['action:changeassetvisibility'] = 'تغيير مرئية الأصل';
+$string['action:duplicateasset'] = 'تكرار الأصل';
+$string['action:changesectionvisibility'] = 'تغيير مرئية القسم';
+$string['action:highlightsectionvisibility'] = 'تمييز مرئية القسم';
+$string['addanewsection'] = 'إنشاء قسم جديد';
+$string['addresourceoractivity'] = 'إنشاء نشاط التعليم';
+$string['admin'] = 'المسؤول';
+$string['advancedbrandingheading'] = 'إدراج علامة تجارية متقدّم';
+$string['ago'] = 'مضت';
+$string['answered'] = 'تمت الإجابة';
+$string['appendices'] = 'الأدوات';
+$string['assigndraft'] = 'مسودة تتطلب تأكيدك';
+$string['assignreopened'] = 'تم إعادة الفتح';
+$string['at'] = 'عند';
+$string['attempted'] = 'تمت المحاولة';
+$string['brandingheading'] = 'إدراج علامات تجارية';
+$string['browse'] = 'استعراض';
+$string['browseallcourses'] = 'استعراض كل المقررات الدراسية';
+$string['cachedef_course_completion_progress'] = 'يستخدم ذلك لتخزين بيانات الإكمال مؤقتًا لكل مقرر دراسي / مستخدم.';
+$string['cachedef_course_completion_progress_ts'] = 'يستخدم ذلك بحيث نستطيع إبطال ذاكرات التخزين المؤقت على مستوى الجلسات إذا تم تغيير إعدادات إكمال المقرر الدراسي لأحد المقررات الدراسية أو الوحدات النمطية.';
+$string['cachedef_webservicedefinitions'] = 'التخزين المؤقت لتعريفات خدمة الويب المنشأة تلقائيًا.';
+$string['changecoverimage'] = 'تغيير صورة الغلاف';
+$string['changefullname'] = 'تغيير اسم الموقع';
+$string['chapters'] = 'الفصول';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h2>محاذاة</h2><img class=img-polaroid src="snap/pix/screenshot.jpg" />';
+$string['conditional'] = 'شرطي';
+$string['contents'] = 'المحتويات';
+$string['contributed'] = 'ممنوح';
+$string['courses'] = 'المقررات الدراسية';
+$string['coursecontacts'] = 'جهات اتصال المقرر الدراسي';
+$string['coursefootertoggle'] = 'إظهار تذييل المقرر الدراسي';
+$string['coursefootertoggledesc'] = 'يعرض تذييل المقرر الدراسي معلومات مفيدة للمستخدمين حول صفحة المقرر الدراسي بما في ذلك جهات اتصال المقرر الدراسي ووصف المقرر الدراسي والنشاط الحديث في المقرر الدراسي.';
+$string['courseformatnotification'] = 'تنسيق المقرر الدراسي الحالي الذي تستخدمه غير مدعوم بالكامل بواسطة النسق "محاذاة". لأفضل الممارسات، يوصي Moodlerooms باستخدام "الموضوعات" أو "تنسيقات الأسبوع للمقرر الدراسي" مع النسق "محاذاة". يمكن تغيير تنسيق المقرر الدراسي في <a href="{$a}">إعدادات المقرر الدراسي</a>.';
+$string['coursefixydefaulttext'] = 'أنت غير مسجل حاليًا في أي مقررات دراسية.<br>سيتم هنا عرض المقررات الدراسية التي أنت مسجل بها.';
+$string['coursenavigation'] = 'تنقل إلى المقرر الدراسي';
+$string['coursesummaryfilesunsuitable'] = 'يرجى تفريغ ملفات تلخيص المقررات الدراسية الخاصة بك قبل محاولة تغيير صورة الغلاف';
+$string['coursetools'] = 'أدوات المقرر الدراسي';
+$string['createsection'] = 'إنشاء قسم';
+$string['current'] = 'حالي';
+$string['customcss'] = 'CSS مخصص';
+$string['customcssdesc'] = 'إضافة أي ملف CSS إضافي لموقعك.';
+$string['deadlines'] = 'المواعيد النهائية';
+$string['deadlinestoggle'] = 'المواعيد النهائية';
+$string['deadlinestoggledesc'] = '"المواعيد النهائية" تعرض أي مواعيد نهائية للأنشطة القادمة الموجودة في المقررات الدراسية التي أنت مسجل بها.';
+$string['defaultsummary'] = 'تُستخدم تلك المساحة لوصف ما يدور حوله هذا الموضوع - بالنص والصورة والصوت والفيديو.';
+$string['defaultintrosummary'] = 'مرحبًا بك في مقررك الدراسي الجديد {$a}.
+<br>ابدأ بوصف ما يدور حوله مقررك الدراسي باستخدام النص والصورة والصوت والفيديو.';
+$string['defaulttopictitle'] = 'موضوع بلا عنوان';
+$string['debugerrors'] = 'تصحيح الأخطاء';
+$string['deleteassetconfirm'] = 'حذف {$a}';
+$string['deletingasset'] = 'حذف {$a}';
+$string['deletingassetname'] = 'حذف {$a->type} "{$a->name}"';
+$string['deletesectionconfirm'] = 'حذف القسم';
+$string['deletingsection'] = 'حذف القسم "{$a}"';
+$string['draft'] = 'لم يتم نشره للطلاب';
+$string['dropzonelabel'] = 'إفلات الملفات للإرفاق أو استعراض <span class="btn-link"></span>';
+$string['due'] = 'مهام مطلوب أداؤها';
+$string['edit'] = 'تحرير "{$a}"';
+$string['editcoursecontent'] = 'تحرير الكتل';
+$string['editcoursesettings'] = 'إعدادات المقرر الدراسي';
+$string['editcoursetopic'] = 'تحرير قسم';
+$string['editcustomfooter'] = 'تحرير التذييل';
+$string['editcustommenu'] = 'تحرير القائمة المخصصة';
+$string['error:coverimageexceedsmaxbytes'] = 'صورة الغلاف تتجاوز الحد الأقصى المسموح به لحجم الملف على مستوى الموقع ({$a})';
+$string['error:coverimageresolutionlow'] = 'لأفضل جودة، نوصي بصورة أكبر بعرض 1024 بيكسل على الأقل.';
+$string['error:failedtochangeassetvisibility'] = 'فشل إخفاء/إظهار الأصل';
+$string['error:failedtochangesectionvisibility'] = 'فشل إخفاء/إظهار القسم';
+$string['error:failedtohighlightsection'] = 'فشل تمييز القسم';
+$string['error:failedtoduplicateasset'] = 'فشل التكرار';
+$string['favicon'] = 'أيقونة المفضلة';
+$string['favicondesc'] = 'يرجى رفع أيقونة المفضلة الخاصة بك بتنسيق ‎.ico هنا. سيتم عرض أيقونة المفضلة إلى جانب عنوان الصفحة في شريط علامات التبويب بالمستعرض وفي الإشارات المرجعية.';
+$string['favorite'] = 'المفضلة {$a}';
+$string['favorited'] = 'مضاف إلى المفضلة {$a}';
+$string['feedbackavailable'] = 'الملاحظات المتاحة';
+$string['feedbacktoggle'] = 'الملاحظات الحديثة';
+$string['feedbacktoggledesc'] = 'الملاحظات الحديثة تعرض أحدث الملاحظات للمتعلِّمين والتقديمات التي تحتاج التقدير للمعلِّمين.';
+$string['footnote'] = 'تذييل الموقع';
+$string['footnotedesc'] = 'تذييل لموقعك. ذلك هو أفضل مكان لوضع ارتباطات للمساعدة والدعم وأي مواقع أخرى تمتلكها مؤسستك وتريد من المتعلمين/المعلمين الوعي بها مثل المكتبة والبريد الإلكتروني وما إلى ذلك.';
+$string['forcepwdwarningpersonalmenu'] = 'عليك <a href="{$a}">تغيير كلمة مرورك</a> قبل استخدام القائمة الشخصية.';
+$string['forumauthor'] = 'المؤلف';
+$string['forumlastpost'] = 'آخر مشاركة';
+$string['forumpicturegroup'] = 'المجموعة';
+$string['forumreplies'] = 'ردود';
+$string['forumtopic'] = 'الموضوع';
+$string['forumposts'] = 'مشاركات المنتدى';
+$string['forumpoststoggle'] = 'مشاركات المنتدى';
+$string['forumpoststoggledesc'] = 'سيتم في القائمة الشخصية عرض أحدث 10 مشاركات بالمنتديات من كل المقررات الدراسية المسجل بها المستخدم الحالي.';
+$string['fullname'] = 'اسم الموقع';
+$string['fullnamedesc'] = 'اسم موقعك.';
+$string['graderadviseuserreport'] = 'لا يعمل "تقرير واضع التقدير" جيدًا على الأجهزة المحمولة. لذا يوصى باستخدام "تقرير المستخدم" بدلاً من ذلك';
+$string['grading'] = 'التقدير';
+$string['headingfont'] = 'خط العنوان';
+$string['headingfont_desc'] = 'يستخدم هذا الخط في العناوين (العناصر h1 - h6) عبر موقعك بأكمله. إذا كنت تقوم بتضمين خط ويب مخصص، فتذكر إضافته إلى نموذج html الخارجي في moodle.';
+$string['help'] = 'مساعدة';
+$string['helpwithlogin'] = 'المساعدة في تسجيل الدخول';
+$string['helpwithloginandguest'] = 'المساعدة في تسجيل الدخول / وصول الضيف';
+$string['hiddencoursestoggle'] = 'المقررات الدراسية المخفية ({$a})';
+$string['highlightedsection'] = 'مميَّز';
+$string['home'] = 'الصفحة الرئيسية';
+$string['image'] = 'صورة';
+$string['introduction'] = 'مقدمة';
+$string['knowledgebase'] = 'قاعدة معارف Moodlerooms';
+$string['loading'] = 'يتم الآن التحميل...';
+$string['loggedinasguest'] = 'أنت مسجل الدخول كضيف';
+$string['loggedoutmsg'] = 'أنت مسجل الخروج حاليًا. إذا كنت تريد متابعة استخدام هذا الموقع، يرجى تسجيل الدخول مرة أخرى.';
+$string['loggedoutmsgtitle'] = 'أنت مسجل الخروج';
+$string['loggedoutfailmsg'] = 'يجب أن تسجل الدخول إلى {$a}.';
+$string['loginform'] = 'تسجيل الدخول';
+$string['logo'] = 'الشعار';
+$string['logodesc'] = 'يرجى رفع شعارك المخصص هنا. سيتم عرض الشعار في رأس الصفحة عبر الموقع بأكمله.';
+$string['menu'] = 'المقررات الدراسية الخاصة بي';
+$string['messageread'] = 'رسالة مقروءة';
+$string['messages'] = 'الرسائل';
+$string['messagestoggle'] = 'الرسائل';
+$string['messagestoggledesc'] = '"الرسائل" تعرض أحدث الرسائل من المستخدمين الآخرين، منذ آخر 12 أسبوعًا.';
+$string['more'] = 'المزيد';
+$string['morenews'] = 'مزيد من الأخبار';
+$string['movingstartedhelp'] = 'انتقل إلى حيث تريد وضع القسم "{$a}"';
+$string['movingdropsectionhelp'] = 'ضع القسم "{$a->moving}" قبل القسم "{$a->before}"';
+$string['moving'] = 'نقل "{$a}"';
+$string['movingcount'] = 'نقل كائنات {$a}';
+$string['movefailed'] = 'فشل نقل "{$a}"';
+$string['move'] = 'نقل "{$a}"';
+$string['movehere'] = 'نقل إلى هنا';
+$string['movesection'] = 'نقل قسم';
+$string['nextsection'] = 'القسم التالي';
+$string['nodeadlines'] = 'ليس لديك مواعيد نهائية قادمة.';
+$string['noforumposts'] = 'ليس لديك مشاركات منتديات ذات صلة.';
+$string['nograded'] = 'ليس لديك ملاحظات حديثة.';
+$string['nograding'] = 'ليس لديك تقديمات للتقدير.';
+$string['nomessages'] = 'ليس لديك رسائل.';
+$string['notanswered'] = 'لم تتم الإجابة عنه';
+$string['notattempted'] = 'لم يتم إجراء محاولة';
+$string['notcontributed'] = 'غير ممنوح';
+$string['notpublished'] = 'لم يتم نشره للطلاب';
+$string['notsubmitted'] = 'غير مقدَّم';
+$string['overdue'] = 'متأخر';
+$string['personalmenu'] = 'ميزات القائمة الشخصية';
+$string['personalmenulogintoggle'] = 'إظهار القائمة الشخصية عند تسجيل الدخول';
+$string['personalmenulogintoggledesc'] = 'فتح القائمة الشخصية مباشرة بعد تسجيل الدخول';
+$string['pld'] = 'PLD';
+$string['pluginname'] = 'محاذاة';
+$string['poster'] = 'صورة الغلاف';
+$string['posterdesc'] = 'صورة كبيرة لصفحة موقعك الأمامية. يجب أن تتضمن الصورة نصًا. تعمل الصور ذات 1280 بيكسل عرض × 500 بيكسل ارتفاع أو أكبر (حتى 2000 بيكسل عرض) بشكل أفضل. سوف يؤدي تحسين الصورة الخاصة بك للويب إلى تسريع وقت تحميل الصفحة.';
+$string['poweredbyrunby'] = 'مدمج مع {$a} من <a href="http://moodlerooms.com/" target="_blank">Moodlerooms</a>، مدعوم بواسطة  <a href="http://www.moodle.com/" target="_blank">Moodle</a>.';
+$string['previoussection'] = 'القسم السابق';
+$string['problemsfound'] = 'تم العثور على مشكلات';
+$string['readmore'] = 'قراءة المزيد&nbsp;»';
+$string['recentactivity'] = 'النشاط الحديث';
+$string['recentfeedback'] = 'ملاحظات';
+$string['region-main'] = 'رئيسي';
+$string['region-side-main-box'] = 'رئيسي';
+$string['region-side-post'] = 'لليمين';
+$string['region-side-pre'] = 'لليسار';
+$string['region-side-top'] = 'أعلى';
+$string['released'] = 'المحرَّر: {$a}';
+$string['reopened'] = 'تم إعادة الفتح';
+$string['seriffont'] = 'خط Serif';
+$string['seriffont_desc'] = 'يُستخدم هذا الخط لمعظم المحتويات المنشأة بواسطة المستخدم. علمًا بأن استخدام خط Serif يجعل المحتويات المنشأة بواسطة المستخدم أكثر قابلية للقراءة وتبدو كما لو أنها مكتوبة بخط اليد.';
+$string['submitted'] = 'تم إرساله';
+$string['subtitle'] = 'وصف الموقع';
+$string['subtitle_desc'] = 'سطر وسم مختصر حول موقعك، حوالي 240 حرفًا في الطول. سيتم عرض وصف موقعك في صفحة الموقع الأمامية.';
+$string['themecolor'] = 'لون العلامة التجارية';
+$string['themecolordesc'] = 'اللون الأساسي لموقعك والذي يتم استخدامه في الارتباطات والأزرار وخلفيات القوائم. علمًا بأن الألوان الساطعة تعمل بشكل أفضل وسوف تمنح موقعك مظهرًا حديثًا.';
+$string['title'] = 'العنوان';
+$string['togglenavigation'] = 'تغيير التنقل';
+$string['topicactions'] = 'إجراءات الموضوع';
+$string['unenrolme'] = 'إلغاء تسجيلي';
+$string['unread'] = 'غير مقروء';
+$string['unsupportedcoverimagetype'] = 'نوع صورة الغلاف ({$a}) غير مدعوم';
+$string['via'] = 'عبر';
+$string['viewcourse'] = 'عرض المقرر الدراسي';
+$string['viewyourprofile'] = 'عرض ملف تعريفك';
+$string['viewmyfeedback'] = 'عرض ملاحظاتي';
+$string['viewcalendar'] = 'عرض تقويمي';
+$string['viewforumposts'] = 'عرض مشاركات منتدياتي';
+$string['viewmessaging'] = 'عرض رسائلي';
+$string['warnsiteformatflexpage'] = 'يستخدم موقعك Flexpage في الصفحة الأمامية. Flexpage غير محسَّن للأجهزة صغيرة الشاشات. من المستحسن أن تقوم بتعطيل استخدام Flexpage في <a href="{$a}">إعدادات الصفحة الأمامية</a> الخاصة بك إن كنت تريد أن يكون موقعك متوافقًا مع الأجهزة المحمولة.';
+$string['xofyanswered'] = '{$a->completed} من أصل {$a->participants} قاموا بالإجابة';
+$string['xofyattempted'] = '{$a->completed} من أصل {$a->participants} قاموا بالمحاولة';
+$string['xofycontributed'] = '{$a->completed} من أصل {$a->participants} قاموا بالمساهمة';
+$string['xofysubmitted'] = '{$a->completed} من أصل {$a->participants} قاموا بالتقديم';
+$string['xungraded'] = '{$a} غير مُقدر';
