9771faf66803aaa7c11772651790ef9b961f27ff
onadjar <oscar.nadjar@blackboard.com>
2018-12-07 19:11:42 +0000

    INT-13888: Changes done according review comments.

--- classes/output/core/course_renderer.php
+++ classes/output/core/course_renderer.php
@@ -389,7 +389,7 @@ class course_renderer extends \core_course_renderer {
         if (!empty($actionsadvanced)) {
             $moreicon = "<img title='".get_string('more', 'theme_snap')."' alt='".get_string('more', 'theme_snap').
                     "' class='svg-icon' src='".$this->output->image_url('more', 'theme')."'/>";
-            $advancedactions = '<div class="dropdown  snap-edit-more-dropdown">';
+            $advancedactions = '<div class="dropdown snap-edit-more-dropdown">';
             $advancedactions .= '<span class="dropdown-toggle snap-edit-asset-more" ';
             $advancedactions .= 'data-toggle="dropdown" data-boundary="window" data-offset="-10,12"';
             $advancedactions .= 'aria-expanded="false" aria-haspopup="true">'.$moreicon.'</span>';
