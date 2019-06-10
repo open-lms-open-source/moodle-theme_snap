acbd87752c36aa9a359ee3bbd475c492ab7b38c4
Rafael Becerra <rafael.becerrarodriguez@blackboard.com>
2019-06-10 15:35:34 +0000

    INT-14188: Add JS for input elements with error after submitting a form with required inputs in blank and add tabindex -1 for not necessary elements in the DOM

--- amd/src/accessibility.js
+++ amd/src/accessibility.js
@@ -23,17 +23,19 @@
 /**
  * JS code to assign attributes and expected behavior for elements in the Dom regarding accessibility.
  */
-define(['jquery', 'core/str'],
-    function($, str) {
+define(['jquery', 'core/str', 'core/event'],
+    function($, str, Event) {
         return {
             init: function() {
 
+                /**
+                 * Module to get the strings from Snap to add the aria-label attribute to new accessibility features.
+                 */
                 str.get_strings([
                     {key : 'accesforumstringdis', component : 'theme_snap'},
                     {key : 'accesforumstringmov', component : 'theme_snap'},
                     {key : 'calendar', component : 'calendar'}
                 ]).done(function(stringsjs) {
-                    // Add strings to needed DOM elements to new accessibility features.
                     $("i.fa-calendar").parent().attr("aria-label", stringsjs[2]);
                     $("input[name='TimeEventSelector[calendar]']").attr('aria-label', stringsjs[2]);
                     if ($("#page-mod-forum-discuss")) {
@@ -42,19 +44,30 @@ define(['jquery', 'core/str'],
                     }
                 });
 
-                // Add accessibility for the tabs and necessary attributes to needed DOM elements to new accessibility features.
                 $(document).ready(function() {
+                    // Add necessary attributes to needed DOM elements to new accessibility features.
                     $("#page-mod-data-edit input[id*='url']").attr("type", "url").attr("autocomplete", "url");
+                    $("#moodle-blocks aside#block-region-side-pre a.sr-only.sr-only-focusable").attr("tabindex", "-1");
+
+                    // Focus first invalid input after a submit is done.
+                    $('.mform').submit(function() {
+                        $('input.form-control.is-invalid:first').focus();
+                    });
+
+                    // Snap tab panels.
                     new Tabpanel("snap-pm-accessible-tab");
                     new Tabpanel("modchooser-accessible-tab");
                 });
 
+                /**
+                 * Add needed accessibility for tabs inside Snap.
+                 * This makes use of Bootstrap accessible tab panel with WAI-ARIA with the arrow keys binding codes.
+                 */
                 function Tabpanel(id) {
                     this._id = id;
                     this.$tpanel = $('#' + id);
                     this.$tabs = this.$tpanel.find('.tab');
                     this.$panels = this.$tpanel.find('.tab-pane');
-
                     this.bindHandlers();
                     this.init();
                 }
@@ -68,17 +81,13 @@ define(['jquery', 'core/str'],
 
                 Tabpanel.prototype.init = function() {
                     var $tab;
-
                     this.$panels.attr('aria-hidden', 'true');
                     this.$panels.removeClass('active in');
-
                     $tab = this.$tabs.filter('.active');
-
                     if ($tab === undefined) {
                         $tab = this.$tabs.first();
                         $tab.addClass('active');
                     }
-
                     this.$tpanel
                         .find('#' + $tab.find('a').attr('aria-controls'))
                         .addClass('active in').attr('aria-hidden', 'false');
@@ -87,32 +96,25 @@ define(['jquery', 'core/str'],
                 Tabpanel.prototype.switchTabs = function($curTab, $newTab) {
                     var $curTabLink = $curTab.find('a'),
                         $newTabLink = $newTab.find('a');
-
                     $curTab.removeClass('active');
                     $curTabLink.attr('tabindex', '-1').attr('aria-selected', 'false');
-
                     $newTab.addClass('active');
                     $newTabLink.attr('aria-selected', 'true');
-
                     this.$tpanel
                         .find('#' + $curTabLink.attr('aria-controls'))
                         .removeClass('active in').attr('aria-hidden', 'true');
-
                     this.$tpanel
                         .find('#' + $newTabLink.attr('aria-controls'))
                         .addClass('active in').attr('aria-hidden', 'false');
-
                     $newTabLink.attr('tabindex', '0');
                     $newTabLink.focus();
                 };
 
                 Tabpanel.prototype.bindHandlers = function() {
                     var self = this;
-
                     this.$tabs.keydown(function(e) {
                         return self.handleTabKeyDown($(this), e);
                     });
-
                     this.$tabs.click(function(e) {
                         return self.handleTabClick($(this), e);
                     });
@@ -120,39 +122,30 @@ define(['jquery', 'core/str'],
 
                 Tabpanel.prototype.handleTabKeyDown = function($tab, e) {
                     var $newTab, tabIndex;
-
                     switch (e.keyCode) {
                         case this.keys.left:
                         case this.keys.up: {
-
                             tabIndex = this.$tabs.index($tab);
-
                             if (tabIndex === 0) {
                                 $newTab = this.$tabs.last();
                             }
                             else {
                                 $newTab = this.$tabs.eq(tabIndex - 1);
                             }
-
                             this.switchTabs($tab, $newTab);
-
                             e.preventDefault();
                             return false;
                         }
                         case this.keys.right:
                         case this.keys.down: {
-
                             tabIndex = this.$tabs.index($tab);
-
                             if (tabIndex === this.$tabs.length-1) {
                                 $newTab = this.$tabs.first();
                             }
                             else {
                                 $newTab = this.$tabs.eq(tabIndex + 1);
                             }
-
                             this.switchTabs($tab, $newTab);
-
                             e.preventDefault();
                             return false;
                         }
@@ -163,6 +156,49 @@ define(['jquery', 'core/str'],
                     var $oldTab = this.$tpanel.find('.tab.active');
                     this.switchTabs($oldTab, $tab);
                 };
+            },
+
+            /**
+             * Custom form error event handler to manipulate the bootstrap markup and show
+             * nicely styled errors in an mform focusing the necessary elements in the form.
+             */
+            enhanceform: function(elementid) {
+                var element = document.getElementById(elementid);
+                $(element).on(Event.Events.FORM_FIELD_VALIDATION, function(event, msg) {
+                    event.preventDefault();
+                    var parent = $(element).closest('.form-group');
+                    var feedback = parent.find('.form-control-feedback');
+                    var invalidinput = parent.find('input.form-control.is-invalid');
+
+                    // Sometimes (atto) we have a hidden textarea backed by a real contenteditable div.
+                    if (($(element).prop("tagName") == 'TEXTAREA') && parent.find('[contenteditable]')) {
+                        element = parent.find('[contenteditable]');
+                    }
+                    if (msg !== '') {
+                        parent.addClass('has-danger');
+                        parent.data('client-validation-error', true);
+                        $(element).addClass('is-invalid');
+                        $(element).attr('aria-describedby', feedback.attr('id'));
+                        $(element).attr('aria-invalid', true);
+                        invalidinput.attr('tabindex', 0);
+                        feedback.html(msg);
+
+                        // Only display and focus when the error was not already visible.
+                        if (!feedback.is(':visible')) {
+                            feedback.show();
+                            feedback.focus();
+                        }
+                    } else {
+                        if (parent.data('client-validation-error') === true) {
+                            parent.removeClass('has-danger');
+                            parent.data('client-validation-error', false);
+                            $(element).removeClass('is-invalid');
+                            $(element).removeAttr('aria-describedby');
+                            $(element).attr('aria-invalid', false);
+                            feedback.hide();
+                        }
+                    }
+                });
             }
         };
     }
---
+++ tests/behat/accessibility_focus_order.feature
@@ -0,0 +1,52 @@
+# This file is part of Moodle - http://moodle.org/
+#
+# Moodle is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Moodle is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Tests for toggle course section visibility in non edit mode in snap.
+#
+# @package    theme_snap
+# @author     Rafael Becerra rafael.becerrarodriguez@blackboard.com
+# @copyright  Copyright (c) 2019 Blackboard Inc. (http://www.blackboard.com)
+# @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+@theme @theme_snap
+Feature: Check that the correct tab order and focus exists for the page.
+
+  Background:
+    Given the following "users" exist:
+      | username  | firstname  | lastname  | email                 |
+      | teacher1  | Teacher    | 1         | teacher1@example.com  |
+    And the following "courses" exist:
+      | fullname | shortname | format |
+      | Course 1 | C1        | topics |
+    And the following "course enrolments" exist:
+      | user      | course  | role            |
+      | teacher1  | C1      | editingteacher  |
+
+  @javascript
+  Scenario: Tabindex -1 exists for unnecessary focus order in the course dashboard.
+    And I log in as "teacher1"
+    And I am on "Course 1" course homepage
+    And I click on "#snap-course-wrapper .toc-footer a:nth-child(2)" "css_element"
+    And the "tabindex" attribute of "//aside[@id='block-region-side-pre']//a[@class='sr-only sr-only-focusable']" "xpath_element" should contain "-1"
+
+  @javascript
+  Scenario: Focus should be over the input with an error after submitting a form with a required field in blank.
+    And I log in as "teacher1"
+    And I am on "Course 1" course homepage
+    And I click on "//li[@id='section-0']//div[@class='content']//div[@class='col-sm-6 snap-modchooser']//a" "xpath_element"
+    And I click on "div.tab-pane.row.text-center.fade.active.in div:nth-child(3) a" "css_element"
+    And I click on "Save and display" "button"
+    Then the focused element is "input.form-control.is-invalid" "css_element"
+
