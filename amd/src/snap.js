a2e1296236d8f96ca7c7ed112edea4c21b7aba25
Rafael Becerra <rafael.becerrarodriguez@blackboard.com>
2019-10-10 19:17:29 +0000

    INT-15215: Modify icon position for Folder activity while in list and card views

--- amd/src/snap.js
+++ amd/src/snap.js
@@ -628,10 +628,6 @@ define(['jquery', 'core/log', 'theme_snap/headroom', 'theme_snap/util', 'theme_s
                         $(this).appendTo($(this).prevAll('.snap-resource-long .contentafterlink .no-overflow'));
                     });
 
-                    // Hide p tags that has multimedia to prevent unnecessary blank spaces in the card content.
-                    $('body.snap-resource-card li.snap-resource.snap-resource-long ' +
-                        'div.contentafterlink div.no-overflow p:has(img)').css('display', 'none');
-
                     // Remove from Dom the completion tracking when it is disabled for an activity.
                     $('.snap-header-card .snap-header-card-icons .disabled-snap-asset-completion-tracking').remove();
 
