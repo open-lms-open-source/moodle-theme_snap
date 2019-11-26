2a61c5b86653440c31e334846a39759a15ce49ff
Jonathan Garcia <jonathan.garcia@blackboard.com>
2019-11-26 15:31:23 -0500

    INT-15220: Version and required version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2019051505;
-$plugin->requires  = 2018120300;
-$plugin->release   = '3.6.3';
+$plugin->requires  = 2019052000;
+$plugin->release   = '3.7.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_snap';
 $plugin->dependencies = [
