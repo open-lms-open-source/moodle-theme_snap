c73a2a7e960d160ec68bf04d5029f790740c34b0
David Castro <david.castro@blackboard.com>
2020-01-21 05:24:11 -0500

    INT-15351: Removal of deprecated themes.
    * theme/bootstrapbase
    * theme/clean
    * theme/more

--- tests/snap_page_requirements_manager_test.php
+++ tests/snap_page_requirements_manager_test.php
@@ -34,14 +34,14 @@ use theme_snap\snap_page_requirements_manager;
 class theme_snap_snap_page_requirements_manager_test extends \advanced_testcase {
 
     /**
-     * Test clean theme does not black list M.core_completion.init.
+     * Test classic theme does not black list M.core_completion.init.
      */
-    public function test_js_init_call_clean() {
+    public function test_js_init_call_classic() {
         global $CFG, $PAGE;
 
         $this->resetAfterTest();
 
-        $CFG->theme = 'clean';
+        $CFG->theme = 'classic';
         $PAGE->initialise_theme_and_output();
 
         $PAGE->requires->js_init_call('M.core_completion.init');
@@ -68,14 +68,14 @@ class theme_snap_snap_page_requirements_manager_test extends \advanced_testcase 
     }
 
     /**
-     * Integration test - Test clean theme does not use snap page requirements manager.
+     * Integration test - Test classic theme does not use snap page requirements manager.
      */
-    public function test_clean_theme_regular_requirements_manager() {
+    public function test_classic_theme_regular_requirements_manager() {
         global $CFG, $PAGE;
 
         $this->resetAfterTest();
 
-        $CFG->theme = 'clean';
+        $CFG->theme = 'classic';
 
         $PAGE->initialise_theme_and_output();
         $this->assertInstanceOf('page_requirements_manager', $PAGE->requires);
