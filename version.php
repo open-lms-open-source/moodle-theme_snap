511528a6f13d4388ad05d4c8f88eb6e3ca1c03c8
Fabian Batioja <fabian.batioja@openlms.net>
2021-06-01 22:14:04 +0000

    INT-17116: Bump versions for 3.10

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2020061103;
-$plugin->requires  = 2020061500;
-$plugin->release   = '3.9.3';
+$plugin->version   = 2021060100;
+$plugin->requires  = 2020110900;
+$plugin->release   = '3.10.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_snap';
 $plugin->dependencies = [
