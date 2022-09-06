fccb1df77e7fed141eb50b131bc0bfc393c5479f
Julian Tovar <julian.tovar@openlms.net>
2022-09-06 11:09:51 -0500

    Revert "INT-18141: Added an SCSS file to style plugins that must follow Course category styles"
    This reverts commit c6d5b67b2dc287a4cee806892d5a3d50bb760d53.

--- lib.php
+++ lib.php
@@ -98,13 +98,11 @@ function theme_snap_set_category_colors($css, $theme) {
 
         $themedirectory = realpath(core_component::get_component_directory('theme_snap'));
         $brandscss = file_get_contents($themedirectory . '/scss/_brandcolor.scss');
-        $pluginscss = file_get_contents($themedirectory . '/scss/_pluginscategory.scss');
         foreach ($categories as $category) {
             $compiler = new core_scss();
             // Rewrite wrapper class with current category id.
             $categoryselector = '.category-' . $category->id . ' {';
             $scss = str_replace('.theme-snap {', $categoryselector, $brandscss);
-            $scss .= str_replace('.theme-snap {', $categoryselector, $pluginscss);
             $compiler->append_raw_scss($scss);
             $compiler->add_variables([
                 'brand-primary' => $colors[$category->id],
@@ -112,9 +110,7 @@ function theme_snap_set_category_colors($css, $theme) {
                 'nav-button-color' => $colors[$category->id],
                 'nav-login-bg' => $colors[$category->id],
                 'nav-login-color' => '#FFFFFF',
-                'custom-menu-text-color' => $dbcustommenutextcoloractive ? $dbcustommenutextcolor : '#FFFFFF',
-                'body-color' => '#565656',
-                'gray-light' => '#6a737b'
+                'custom-menu-text-color' => $dbcustommenutextcoloractive ? $dbcustommenutextcolor : '#FFFFFF'
             ]);
 
             try {
