88b65d597aaa60248acc9d2a299e602e543f083c
Bryan Cruz <bryan.cruz@openlms.net>
2023-04-11 17:08:30 +0000

    INT-18711: 4.0 MP2 Version bump

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2023012400;
+$plugin->version   = 2023041100;
 $plugin->requires  = 2022041900;
-$plugin->release   = '4.0.5';
+$plugin->release   = '4.0.7';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_snap';
 $plugin->dependencies = [
