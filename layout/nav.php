347b29c2cb822071bdb9928de0c82cf1cc178f67
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2024-03-01 11:38:22 -0500

    INT-19525:  Change Snap feeds icon and add separator

--- layout/nav.php
+++ layout/nav.php
@@ -75,6 +75,9 @@ echo html_writer::link($defaulthomeurl, $sitefullname, $attrs);
     echo core_renderer::search_box();
     echo '</span>';
     echo $OUTPUT->snap_feeds_side_menu_trigger();
+    if (is_siteadmin()) {
+        echo '<div class="snap_line_separator"></div>';
+    }
     echo $OUTPUT->render($settingslink);
     ?>
 </div>
