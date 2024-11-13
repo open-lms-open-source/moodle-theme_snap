96a11470b5eb9cbc814d84b3df091a4ae0009dd4
Bryan Cruz <bryan.cruz@openlms.net>
2024-11-13 16:39:09 -0500

    INT-20194: Pre Sandbox - 4.4 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024091701;
-$plugin->requires  = 2023100900;
-$plugin->release   = '4.3.4';
+$plugin->version   = 2024111300;
+$plugin->requires  = 2024042200;
+$plugin->release   = '4.4.4';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_snap';
 $plugin->dependencies = [
