ed1c1f55d1e2e9ce277dae9b6d9358cb96af3218
Julian Tovar <julian.tovar@openlms.net>
2025-04-25 11:00:53 -0500

    Revert "INT-20993 theme_snap: Fixed the drawer display width to avoid overlapping the rest of the UI"
    This reverts commit 31a62736308ba06085ba494f29cc305890576880.

--- amd/src/snap.js
+++ amd/src/snap.js
@@ -543,14 +543,12 @@ define(['jquery', 'core/log', 'core/aria', 'theme_snap/headroom', 'theme_snap/ut
             });
 
             // Admin drawer: Onclick for toggle of state-visible of admin block and mobile menu.
-            $(document).on("click", "#admin-menu-trigger, #toc-mobile-menu-toggle, [id^=\"message-drawer-toggle-\"]", function(e) {
-
+            $(document).on("click", "#admin-menu-trigger, #toc-mobile-menu-toggle", function(e) {
                 var href = this.getAttribute('href');
                 // Make this only happen for settings button.
-                if (this.getAttribute('id') === 'admin-menu-trigger'
-                    || this.getAttribute('id').startsWith('message-drawer-toggle-')) {
+                if (this.getAttribute('id') === 'admin-menu-trigger') {
                     $(this).toggleClass('active');
-                    $('#page').toggleClass('show-drawer-right');
+                    $('#page').toggleClass('offcanvas');
                     if ($(this).attr('aria-expanded') === 'true') {
                         $(this).attr('aria-expanded', false);
                     } else {
@@ -583,7 +581,7 @@ define(['jquery', 'core/log', 'core/aria', 'theme_snap/headroom', 'theme_snap/ut
                 var href = this.getAttribute('href');
                 if (this.getAttribute('id') === 'snap_feeds_side_menu_trigger') {
                     $(this).toggleClass('active');
-                    $('#page').toggleClass('show-drawer-right');
+                    $('#page').toggleClass('offcanvas');
                     if ($(this).attr('aria-expanded') === 'true') {
                         $(this).attr('aria-expanded', false);
                     } else {
