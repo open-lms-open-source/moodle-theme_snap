9fe02f1cf2af0cd0df74dcadafe77c6ada7ec625
Julian Tovar <julian.tovar@openlms.net>
2025-09-03 10:49:38 -0500

    INT-21293 theme_snap: Fixed the hover style on the Reset all button, and the responsiveness for the fa-beat animation which was causing a misbehavior

--- amd/src/accessibility.js
+++ amd/src/accessibility.js
@@ -403,6 +403,9 @@ define(['jquery', 'core/str', 'core/event', 'core_form/events', 'theme_boost/boo
                         accessibilityIcon.querySelector('button').addEventListener('blur', () => {
                             accessibilityWrapper.classList.remove('focused-icon');
                         });
+                        document.querySelector('.usermenu a').addEventListener('click', () => {
+                            accessibilityPanel.style.display = 'none';
+                        });
 
                         if (lineSeparator) {
                             lineSeparator.parentElement.insertBefore(accessibilityWrapper, lineSeparator);
